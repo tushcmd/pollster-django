ebf333c5c0a904826479f1c31920ed1639e2a4c9
tushcmd <muturidavid854@gmail.com>
2024-07-23 04:51:53 +0300

    polls working

--- .history/pollster/polls/views_20240723044440.py
+++ .history/pollster/polls/views_20240723044440.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
